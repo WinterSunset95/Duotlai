dd6706ff0c512c0a66f972e064cb0edb627fbd3d
WinterSunset95 <shirookamicod@gmail.com>
2022-03-19 22:31:50 -0700

    changes to the icon files

--- english/src/about.js
+++ english/src/about.js
@@ -2,9 +2,9 @@ var prevScrollPos = window.pageYOffset;
 window.onscroll = function () {
     var currentScrollPos = window.pageYOffset;
     if (prevScrollPos > currentScrollPos) {
-        document.getElementById("footer").style.bottom = "-50px"; 
+        document.getElementById("footer").style.bottom = "-70px"; 
     } else {
-        document.getElementById("footer").style.bottom = "10px"
+        document.getElementById("footer").style.bottom = "5px"
     }
     prevScrollPos = currentScrollPos;
 }
